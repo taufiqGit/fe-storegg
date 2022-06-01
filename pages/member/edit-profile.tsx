24b8671f5fd782a7ba30c8dfaaf988c404db0277
taufiqGit <wecannn29@gmail.com>
2022-06-01 11:20:46 +0700

    resolve responsive member page 2

--- pages/member/edit-profile.tsx
+++ pages/member/edit-profile.tsx
@@ -30,9 +30,9 @@ export default function EditProfile() {
     return(
     <section className="edit-profile overflow-auto">
         <SideBar activeMenu="edit-profile"/>
-        <main className="main-wrapper wajib" style={{marginLeft: '340px'}}>
+        <main className="main-wrapper wajib">
             <div className="ps-lg-0">
-                <h2 className="text-4xl fw-bold color-palette-1 mb-30">Settings</h2>
+                <h2 className="text-4xl fw-bold color-palette-1 mt-20 mb-30">Settings</h2>
                 <div className="bg-card pt-30 ps-30 pe-30 pb-30">
                     <form action="">
                         <div className="photo d-flex">
